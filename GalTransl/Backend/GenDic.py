2424c17f8b5163cca023fd9f29a5ecf4d8859ee6
cx2333 <99069487+xd2333@users.noreply.github.com>
2025-04-18 23:22:00 +0800

    bug fix

--- GalTransl/Backend/GenDic.py
+++ GalTransl/Backend/GenDic.py
@@ -8,16 +8,17 @@ from alive_progress import alive_bar
 from GalTransl.COpenAI import COpenAITokenPool
 from GalTransl.ConfigHelper import CProxyPool
 from GalTransl import LOGGER, LANG_SUPPORTED
-from GalTransl.i18n import get_text,GT_LANG
+from GalTransl.i18n import get_text, GT_LANG
 from sys import exit
 from GalTransl.ConfigHelper import CProjectConfig
 from GalTransl.Dictionary import CGptDict
-from GalTransl.Utils import contains_katakana,is_all_chinese,decompress_file_lzma
+from GalTransl.Utils import contains_katakana, is_all_chinese, decompress_file_lzma
 from GalTransl.Backend.BaseTranslate import BaseTranslate
-from GalTransl.Backend.Prompts import GENDIC_PROMPT,GENDIC_SYSTEM,H_WORDS_LIST
+from GalTransl.Backend.Prompts import GENDIC_PROMPT, GENDIC_SYSTEM, H_WORDS_LIST
 import collections
 from typing import List, Set, Dict, Optional
 from threading import Lock
+
 
 class GenDic(BaseTranslate):
     def __init__(
@@ -28,51 +29,49 @@ class GenDic(BaseTranslate):
         token_pool: COpenAITokenPool,
     ):
         super().__init__(config, eng_type, proxy_pool, token_pool)
-        self.dic_counter=collections.Counter()
-        self.dic_list=[]
-        self.wokers=config.getKey("workersPerProject")
+        self.dic_counter = collections.Counter()
+        self.dic_list = []
+        self.wokers = config.getKey("workersPerProject")
         self.counter_lock = Lock()
         self.list_lock = Lock()
-        self.config=config
+        self.config = config
+        self.init_chatbot(eng_type, config)
         pass
 
-
-
-    async def llm_gen_dic(self,text:str,name_list=[]):
-        hint="无"
-        name_hit=[]
+    async def llm_gen_dic(self, text: str, name_list=[]):
+        hint = "无"
+        name_hit = []
         for name in name_list:
             if name in text:
                 name_hit.append(name)
         if name_hit:
-            hint="输入文本中的这些词语是一定要加入术语表的: \n"+"\n".join(name_hit)
+            hint = "输入文本中的这些词语是一定要加入术语表的: \n" + "\n".join(name_hit)
 
-        prompt=GENDIC_PROMPT.format(input=text,hint=hint)
-        rsp=await self.ask_chatbot(prompt,GENDIC_SYSTEM)
+        prompt = GENDIC_PROMPT.format(input=text, hint=hint)
+        rsp = await self.ask_chatbot(
+            prompt=prompt, system=GENDIC_SYSTEM, temperature=0.6
+        )
         print(rsp)
-        lines=rsp.split("\n")
+        lines = rsp.split("\n")
 
         for line in lines:
-            sp=line.split("\t")
-            if len(sp)<3:
+            sp = line.split("\t")
+            if len(sp) < 3:
                 continue
             if "日文" in sp[0]:
                 continue
-            src=sp[0]
-            dst=sp[1]
-            note=sp[2]
+            src = sp[0]
+            dst = sp[1]
+            note = sp[2]
             with self.counter_lock:
                 if src in self.dic_counter:
-                    self.dic_counter[src]+=1
-                    if self.dic_counter[src]==2:
+                    self.dic_counter[src] += 1
+                    if self.dic_counter[src] == 2:
                         print(f"{src}\t{dst}\t{note}")
                 else:
-                    self.dic_counter[src]=1
+                    self.dic_counter[src] = 1
                     with self.list_lock:
-                        self.dic_list.append([src,dst,note])
-
-                    
-
+                        self.dic_list.append([src, dst, note])
 
     async def batch_translate(
         self,
@@ -82,88 +81,96 @@ class GenDic(BaseTranslate):
         with alive_bar(title="载入分词……") as bar:
             # get tmp dir
             import tempfile
-            tmp_dir=tempfile.gettempdir()
-            model_path=os.path.join(tmp_dir,"bccwj-suw+unidic_pos+pron.model")
+
+            tmp_dir = tempfile.gettempdir()
+            model_path = os.path.join(tmp_dir, "bccwj-suw+unidic_pos+pron.model")
             if not os.path.exists(model_path):
-                zst_path="./res/bccwj-suw+unidic_pos+pron.model.xz"
-                decompress_file_lzma(zst_path,model_path)
+                zst_path = "./res/bccwj-suw+unidic_pos+pron.model.xz"
+                decompress_file_lzma(zst_path, model_path)
             bar()
             import vaporetto
+
             try:
-                with open(model_path, 'rb') as fp:
+                with open(model_path, "rb") as fp:
                     model = fp.read()
-                tokenizer = vaporetto.Vaporetto(model, predict_tags = True)
+                tokenizer = vaporetto.Vaporetto(model, predict_tags=True)
             except Exception as e:
                 LOGGER.error(e)
                 LOGGER.error("载入分词模型失败，请尝试重启程序")
                 os.remove(model_path)
                 return False
             bar()
-            
-            word_counter=collections.Counter()
-            segment_list=[]
-            segment_words_list=[]
-            name_set=set()
-            max_len=512
-            tmp_text=""
+
+            word_counter = collections.Counter()
+            segment_list = []
+            segment_words_list = []
+            name_set = set()
+            max_len = 512
+            tmp_text = ""
             for item in json_list:
-                if len(tmp_text)>max_len:
+                if len(tmp_text) > max_len:
                     segment_list.append(tmp_text)
-                    tmp_text=""
-                if "name" in item and item["name"]!="":
+                    tmp_text = ""
+                if "name" in item and item["name"] != "":
                     name_set.add(item["name"])
-                    tmp_text+=item["name"]+item["message"]+"\n"
-                    word_counter[item["name"]]+=2
+                    tmp_text += item["name"] + item["message"] + "\n"
+                    word_counter[item["name"]] += 2
                 else:
-                    tmp_text+=item["message"]+"\n"
-            
+                    tmp_text += item["message"] + "\n"
+
             segment_list.append(tmp_text)
-            bar.title="处理分词……"
+            bar.title = "处理分词……"
 
             for item in segment_list:
-                tmp_words=set()
+                tmp_words = set()
                 tokens = tokenizer.tokenize(item)
                 for token in tokens:
-                    surf=token.surface()
-                    tag=token.tag(0)
-                    if len(surf)<=1:
+                    surf = token.surface()
+                    tag = token.tag(0)
+                    if len(surf) <= 1:
                         continue
                     if is_all_chinese(surf):
                         continue
                     if tag is None:
                         if contains_katakana(surf):
                             tmp_words.add(surf)
-                            word_counter[surf]+=1
+                            word_counter[surf] += 1
                     if tag and "固有名詞" in tag:
                         tmp_words.add(surf)
-                        word_counter[surf]+=1
+                        word_counter[surf] += 1
                 segment_words_list.append(tmp_words)
                 bar()
-        
+
         # 剔除出现次数小于2的词语
-        word_counter = {word: count for word, count in word_counter.items() if count >= 2}
-        segment_words_list_new=[]
+        word_counter = {
+            word: count for word, count in word_counter.items() if count >= 2
+        }
+        segment_words_list_new = []
         for item in segment_words_list:
-            item_new=set()
+            item_new = set()
             for word in item:
                 if word in word_counter:
                     item_new.add(word)
             segment_words_list_new.append(item_new)
 
-        index_list=solve_sentence_selection(segment_words_list_new)
+        index_list = solve_sentence_selection(segment_words_list_new)
         # 取前100个
-        index_list=index_list[:128]
-        LOGGER.info(f"启动{self.wokers}个工作线程，共{len(index_list)}个任务" )
+        index_list = index_list[:128]
+        LOGGER.info(f"启动{self.wokers}个工作线程，共{len(index_list)}个任务")
         sem = asyncio.Semaphore(self.wokers)
+
         async def process_item_async(idx):
             async with sem:
                 try:
                     item = segment_list[idx]
-                    await self.llm_gen_dic(item,name_list=list(name_set))
+                    await self.llm_gen_dic(item, name_list=list(name_set))
                 except Exception as e:
                     LOGGER.error(f"处理任务时出错: {e}")
+
         tasks = [process_item_async(idx) for idx in index_list]
-        with alive_bar(total=len(index_list),title=f"{self.wokers} 线程生成字典中……") as bar:
+        with alive_bar(
+            total=len(index_list), title=f"{self.wokers} 线程生成字典中……"
+        ) as bar:
             for f in asyncio.as_completed(tasks):
                 await f
                 bar()
@@ -171,7 +178,7 @@ class GenDic(BaseTranslate):
         # 保存到文件
         # 按出现次数排序
         self.dic_list.sort(key=lambda x: self.dic_counter[x[0]], reverse=True)
-        final_list=[]
+        final_list = []
         # 过滤只出现1次的词语
         for item in self.dic_list:
             if "NULL" in item[0]:
@@ -179,7 +186,7 @@ class GenDic(BaseTranslate):
             if item[0] in H_WORDS_LIST:
                 continue
 
-            if self.dic_counter[item[0]]>1:
+            if self.dic_counter[item[0]] > 1:
                 final_list.append(item)
             elif "人名" in item[2]:
                 final_list.append(item)
@@ -190,16 +197,15 @@ class GenDic(BaseTranslate):
             elif item[0] in name_set:
                 final_list.append(item)
 
-        result_path=os.path.join(self.config.getProjectDir(),"项目GPT字典-生成.txt")
+        result_path = os.path.join(self.config.getProjectDir(), "项目GPT字典-生成.txt")
 
         with open(result_path, "w", encoding="utf-8") as f:
             f.write("# 格式为日文[Tab]中文[Tab]解释(可不写)，参考项目wiki\n")
             for item in final_list:
-                f.write(item[0]+"\t"+item[1]+"\t"+item[2]+"\n")
+                f.write(item[0] + "\t" + item[1] + "\t" + item[2] + "\n")
         LOGGER.info(f"字典生成完成，共{len(final_list)}个词语，保存到{result_path}")
 
         return True
-
 
 
 def solve_sentence_selection(sentences):
@@ -232,4 +238,3 @@ def solve_sentence_selection(sentences):
             break
 
     return selected_indices
-
