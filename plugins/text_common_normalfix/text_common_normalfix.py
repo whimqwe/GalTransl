05b00ed0669fdb1b6f3daac2094fbb25fbb77770
cx2333 <99069487+xd2333@users.noreply.github.com>
2025-04-24 22:39:46 +0800

    fix(text_common_normalfix): 修复文本处理中换行符的处理逻辑
    在处理文本时，新增了对开头和结尾换行符的处理逻辑，确保换行符被正确移动到左右符号中，避免文本处理时丢失换行符信息。

--- plugins/text_common_normalfix/text_common_normalfix.py
+++ plugins/text_common_normalfix/text_common_normalfix.py
@@ -11,9 +11,21 @@ class text_common_normalfix(GTextPlugin):
         if tran.post_jp.endswith("　"):
             tran.post_jp = tran.post_jp[:-1]
             tran.right_symbol = "　" + tran.right_symbol
+
+        if tran.post_jp.startswith("\\n"):
+            tran.post_jp = tran.post_jp[2:]
+            tran.left_symbol = tran.left_symbol + "\\n"
         if tran.post_jp.endswith("\\n"):
             tran.post_jp = tran.post_jp[:-2]
             tran.right_symbol = "\\n" + tran.right_symbol
+            
+        if tran.post_jp.startswith("\n"):
+            tran.post_jp = tran.post_jp[1:]
+            tran.left_symbol = tran.left_symbol + "\n"
+        if tran.post_jp.endswith("\n"):
+            tran.post_jp = tran.post_jp[:-1]
+            tran.right_symbol = "\n" + tran.right_symbol
+
         return tran
 
     def after_src_processed(self, tran: CSentense) -> CSentense:
